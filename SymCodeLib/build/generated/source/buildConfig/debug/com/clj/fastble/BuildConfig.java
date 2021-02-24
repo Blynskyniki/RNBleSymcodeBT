649a99a07dc6508ca0cb8808e7f981dfcff85997
Niki <blynskijno@lad24.ru>
2021-02-24 15:17:40 +0300

    feat: Init

---
+++ SymCodeLib/build/generated/source/buildConfig/debug/com/clj/fastble/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.clj.fastble;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.clj.fastble";
+  public static final String BUILD_TYPE = "debug";
+}
