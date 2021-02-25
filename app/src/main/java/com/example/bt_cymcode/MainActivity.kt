6cbeada9a87a9f7b36662435a18c0eec62a4fedf
Niki <blynskijno@lad24.ru>
2021-02-25 08:31:47 +0300

    feat: add Jet Pack Compose

--- app/src/main/java/com/example/bt_cymcode/MainActivity.kt
+++ app/src/main/java/com/example/bt_cymcode/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.bt_cymcode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("Hello world!")
+        }
     }
 }
